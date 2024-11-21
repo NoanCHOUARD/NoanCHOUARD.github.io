9266fc0e7ae91cd1fef9e9c36c56420405c8406f
NoanCHOUARD <noan.chouard@etu.unilim.fr>
2024-11-21 08:50:12 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://NoanCHOUARD.github.io',
+    integrations: [mdx(), sitemap(), tailwind()],
 });
+
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'Retro': "Retro-Gaming, sans-serif",
+				'Rubik': "Rubik, sans-serif",
+		},
+	},
+	plugins: [],
+}
+
+}
+
+
+
+
