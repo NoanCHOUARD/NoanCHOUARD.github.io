f84cd9884ef9bbafe1388ffa240368a4948b91f6
NoanCHOUARD <noan.chouard@etu.unilim.fr>
2024-11-21 09:05:11 +0100

    Test3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https:NoanCHOUARD/Noan.CHOUARD.github.io',
+    site: 'https://NoanCHOUARD.github.io',
     integrations: [mdx(), sitemap(), tailwind()],
 });
 
