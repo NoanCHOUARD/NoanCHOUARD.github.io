2018d6590121263b2eb07f816c07b9be52797ce5
NoanCHOUARD <144430993+NoanCHOUARD@users.noreply.github.com>
2025-01-17 15:33:33 +0100

    new-site-two

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -28,14 +28,22 @@ export default {
 		},
 	plugins: [],
 	extend: {
-		keyframes: {
-			translation: {
-
-				},
-
-
-			}
+		animation: {
+		  text: 'text 5s ease infinite',
 		},
+		keyframes: {
+		  text: {
+			'0%, 100%': {
+			  'background-size': '200% 200%',
+			  'background-position': 'left center',
+			},
+			'50%': {
+			  'background-size': '200% 200%',
+			  'background-position': 'right center',
+			},
+		  },
+		},
+	  },
 	}
 
 }
